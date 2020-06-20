4a8366f9f259455d1939e1b81193817d011ee8a6
contexD <maxconte232@gmail.com>
2020-06-20 17:50:58 +0200

    added hook to model; password gets hashed now before user is created in db

--- models/user.js
+++ models/user.js
@@ -1,4 +1,6 @@
 "use strict";
+import bcrypt from "bcrypt";
+
 module.exports = (sequelize, DataTypes) => {
   const user = sequelize.define(
     "user",
@@ -32,6 +34,15 @@ module.exports = (sequelize, DataTypes) => {
   user.findByLogin = async (login) => {
     const result = await user.findOne({ where: { email: login } });
     return result;
+  };
+
+  user.beforeCreate(async (userInstance) => {
+    userInstance.password = await userInstance.generatePasswordHash();
+  });
+
+  user.prototype.generatePasswordHash = async function () {
+    const saltRounds = 10;
+    return await bcrypt.hash(this.password, saltRounds);
   };
 
   return user;
