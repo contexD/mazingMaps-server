233d696ea7368ae331f3ee7c9e9aa7b96ef18938
contexD <maxconte232@gmail.com>
2020-07-12 20:52:18 +0200

    Changes schema to comply to new data model

--- src/schema/edge.js
+++ src/schema/edge.js
@@ -3,12 +3,14 @@ const { gql } = require("apollo-server-express");
 const edgeSchema = gql`
   extend type Mutation {
     createEdge(sourceId: ID!, targetId: ID!): EdgeMutationResponse!
-    deleteEdge(sourceId: ID!, targetId: ID!): EdgeMutationResponse!
+    deleteEdge(id: ID!): EdgeMutationResponse!
   }
 
   type Edge {
+    id: String!
     source: Vertex!
     target: Vertex!
+    animated: Boolean
   }
 
   type EdgeMutationResponse implements MutationResponse {
